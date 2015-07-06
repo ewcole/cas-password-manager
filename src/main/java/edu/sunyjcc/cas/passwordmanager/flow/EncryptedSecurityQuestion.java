7432b08ad75a0a2cf61fb8b4638d43943f4c2c64
Ed Cole <edcole@mail.sunyjcc.edu>
2015-07-06 14:22:18 -0400

    Add encrypted access attributes to EncryptedSecurityQuestion.java

--- src/main/java/edu/sunyjcc/cas/passwordmanager/flow/EncryptedSecurityQuestion.java
+++ src/main/java/edu/sunyjcc/cas/passwordmanager/flow/EncryptedSecurityQuestion.java
@@ -15,11 +15,48 @@ public class EncryptedSecurityQuestion extends SecurityQuestion {
 
     private static final long serialVersionUID = 1L;
     
+    /** No-argument constructor */
     public EncryptedSecurityQuestion() { }
     
+    /** Constructor with encrypted question text and response */
     public EncryptedSecurityQuestion(String encryptedQuestionText, String encryptedResponseText) {
         this.setEncryptedQuestionText(encryptedQuestionText);
         this.setEncryptedResponseText(encryptedResponseText);
+    }
+
+
+    /** Return an encrypted form of the question text for storage */
+    @Override
+    public String getEncryptedQuestionText() {
+        // The default version treats it as clear text.
+        return this.getQuestionText();
+    }
+
+    /** Decrypt the encryptedQuestionText and set questionText to that value.
+     *  @param encryptedQuestionText The value which, when decrypted, yeilds
+     *                               the user's question text.
+     */
+    @Override
+    public void setEncryptedQuestionText(String encryptedQuestionText) {
+        // The default version treats it as clear text.
+        this.setQuestionText(encryptedQuestionText);
+    }
+
+    /** Return an encrypted form of the response text for storage */
+    @Override
+    public String getEncryptedResponseText() {
+        // The default version treats it as clear text.
+        return this.getResponseText();
+    }
+
+    /** Decrypt the encryptedResponseText and set responseText to that value.
+     *  @param encryptedResponseText The value which, when decrypted, yeilds
+     *                               the user's response text.
+     */
+    @Override
+    public void setEncryptedResponseText(String encryptedResponseText) {
+        // The default version treats it as clear text.
+        this.setResponseText(encryptedResponseText);
     }
 
 }
